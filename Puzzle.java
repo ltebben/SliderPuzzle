458360167023fa7167bb8f7d3cc44c7a2bedc3ce
Laura Tebben <ltebben09@gmail.com>
2016-09-04 21:55:24 -0400

    Added some display commands

--- Puzzle.java
+++ Puzzle.java
@@ -1,24 +1,30 @@
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
 
 public class Puzzle {
+
 	static BufferedImage readImage(){
 		BufferedImage img = null;
 		try {
 			img = ImageIO.read(new File("cat.jpg"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Could not read Image");
 		}
 		return img;		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BufferedImage img = readImage();
-		img.setVisible(true);
+		Graphics graph = img.getGraphics();
+		graph.drawImage(img, 0, 0, null);
+		
 	}
 
 }
