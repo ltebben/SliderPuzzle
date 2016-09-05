d1b2c1bbb17ab1d8f4a6263fe072cfc6ff693176
Laura Tebben <ltebben09@gmail.com>
2016-09-05 00:53:32 -0400

    Set block to white and added getNumRows and getNumCols

--- puzzle/ImageDetails.java
+++ puzzle/ImageDetails.java
@@ -3,9 +3,14 @@ package puzzle;
 import java.awt.image.BufferedImage;
 
 public class ImageDetails{
-	ImageDetails(BufferedImage img){width = img.getWidth(); height = img.getHeight();}
 	private int width;
 	private int height;
+	
+	ImageDetails(BufferedImage img){
+		width = img.getWidth(); 
+		height = img.getHeight();
+	}
+	
 	public int GetWidth(){return width;}
 	public int GetHeight(){return height;}
 }
--- puzzle/SplitImage.java
+++ puzzle/SplitImage.java
@@ -8,24 +8,29 @@ import javax.swing.ImageIcon;
 
 
 public class SplitImage{
-	public ArrayList<ImageIcon> imgIcons = new ArrayList<ImageIcon>();
+	private ArrayList<ImageIcon> imgIcons = new ArrayList<ImageIcon>();
+	private int numRows, numCols;
 
-
+	//constructor
 	SplitImage(BufferedImage img){
+		//get dimensions
 		ImageDetails det = new ImageDetails(img);
 		int width = det.GetWidth();
 		int height = det.GetHeight();
 
-		int numRows = 3;
-		int numCols = 3;
-
+		numRows = 3;
+		numCols = 3;
+		
+		//create chunk dimensions
 		int chunkWidth = width / numCols;
 		int chunkHeight = height / numRows;
-
+		
+		//Splitting the image into blocks and adding blocks into array of imageIcons
 		for (int row = 0; row < numRows; row++) {
 			for (int col = 0; col < numCols; col++) {
 				
 				if(row == 0 && col == 0){
+					//Set top left block to white
 					BufferedImage whiteBlock = new BufferedImage(chunkWidth, chunkHeight, BufferedImage.TYPE_INT_ARGB);
 					Color White = new Color(255, 255, 255); 
 					int rgb = White.getRGB();
@@ -35,6 +40,7 @@ public class SplitImage{
 							whiteBlock.setRGB(r, c, rgb);
 						}
 					}
+					
 					imgIcons.add(new ImageIcon(whiteBlock));
 				}
 				
@@ -46,5 +52,7 @@ public class SplitImage{
 	}
 
 	public ArrayList<ImageIcon> getImgIcons(){return imgIcons;}
-
+	
+	public int getNumRows(){return numRows;}
+	public int getNumCols(){return numCols;}
 }
