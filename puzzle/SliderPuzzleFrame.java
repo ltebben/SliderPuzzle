31047d4c87e2222ffae4e4f5ef0daad397864154
Zach Collins <collinzy@mail.uc.edu>
2016-09-06 23:10:47 -0400

    Adding SliderPuzzleFrame.java

---
+++ puzzle/SliderPuzzleFrame.java
@@ -0,0 +1,17 @@
+package puzzle;
+
+import javax.swing.JFrame;
+
+public class SliderPuzzleFrame extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public SplitImage splits;
+	
+	SliderPuzzleFrame(String str) {
+		super(str);
+	}
+}
