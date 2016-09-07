3c6de208cafddfffa2e25a7d829807d7a15da9ed
Zach Collins <collinzy@mail.uc.edu>
2016-09-06 21:00:29 -0400

    Add mouse listening functionality

---
+++ puzzle/SliderPuzzleLabel.java
@@ -0,0 +1,37 @@
+package puzzle;
+
+import java.awt.GridBagConstraints;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class SliderPuzzleLabel extends JLabel {
+	
+	/**
+	 * Extension of JLabel to let it own a GridBagConstraints
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	
+	private GridBagConstraints gridCons;
+	
+	SliderPuzzleLabel(ImageIcon img) {
+		super(img);
+	}
+	
+	public void addConstraints(GridBagConstraints gridConstraints) {
+		gridCons = gridConstraints;
+	}
+	
+	public GridBagConstraints getConstraints() {
+		return gridCons;
+	}
+	
+	public int getGridX() { 
+		return gridCons.gridx;
+	}
+	
+	public int getGridY() { 
+		return gridCons.gridy;
+	}
+}
